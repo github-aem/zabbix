d69005924efb54ee5a3d16cb5e48d672f3a4bced
github-aem <121136880+github-aem@users.noreply.github.com>
2024-07-25 14:12:06 -0700

    Update Zabbix.AEM.RasPi.sh

--- Zabbix.AEM.RasPi.sh
+++ Zabbix.AEM.RasPi.sh
@@ -1,3 +1,6 @@
+#Zabbix REMOTE install - - OUTSIDE office using standard DNS / Internet Connectivity##
+#Wget script - Chmod +X scriptname.sh
+#run script as root and ensure SUDO is installed on guest
 wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-2+debian11_all.deb
 apt install ./zabbix-release_7.0-2+debian11_all.deb
 apt update
@@ -9,9 +12,11 @@ sudo sed -i -e 's/Hostname=Zabbix\ server/#Hostname=Zabbix\ server/' /etc/zabbix
 sudo sed -i -e 's/#\ HostnameItem=system.hostname/HostnameItem=system.hostname/' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i -e 's/#\ HostMetadata=/HostMetadata=PIES/' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i -e 's/#\ HostMetadataItem=/HostMetadataItem=:PIES/' /etc/zabbix/zabbix_agent2.conf
+sudo adduser zabbix docker
 sudo systemctl restart zabbix-agent2
 sudo systemctl enable zabbix-agent2
 #sudo systemctl status zabbix-agent2
-sudo adduser zabbix docker
+#check /etc/zabbix/zabbix_agent2.conf for changes and reboot to make sure all works
+#for 2 way comms make sure TCP 10050 is open to the guest
 echo "Zabbix Setup Complete" & hostname
-
+#end
