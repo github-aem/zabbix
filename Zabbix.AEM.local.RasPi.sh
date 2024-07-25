3eb035e89b4abe658c995fb447c2eb944e328b49
github-aem <121136880+github-aem@users.noreply.github.com>
2024-07-25 14:09:37 -0700

    Update Zabbix.AEM.local.RasPi.sh

--- Zabbix.AEM.local.RasPi.sh
+++ Zabbix.AEM.local.RasPi.sh
@@ -1,3 +1,5 @@
+#Zabbix Agent 7 - LOCAL OFFICE INSTALLER - - 10.1.1.0/24 IP Space Systems ONLY or VPN Connected#
+#Run script as root and ensure SUDO is installed#
 wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-2+debian11_all.deb
 apt install ./zabbix-release_7.0-2+debian11_all.deb
 apt update
@@ -9,9 +11,10 @@ sudo sed -i -e 's/Hostname=Zabbix\ server/#Hostname=Zabbix\ server/' /etc/zabbix
 sudo sed -i -e 's/#\ HostnameItem=system.hostname/HostnameItem=system.hostname/' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i -e 's/#\ HostMetadata=/HostMetadata=PIES/' /etc/zabbix/zabbix_agent2.conf
 sudo sed -i -e 's/#\ HostMetadataItem=/HostMetadataItem=:PIES/' /etc/zabbix/zabbix_agent2.conf
+sudo adduser zabbix docker
 sudo systemctl restart zabbix-agent2
 sudo systemctl enable zabbix-agent2
 #sudo systemctl status zabbix-agent2
-sudo adduser zabbix docker
+#end of script - - check /etc/zabbix/zabbix_agent2.conf for changes#
 echo "Zabbix Setup Complete" & hostname
 
